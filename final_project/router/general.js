4541db507af5d54346532af88fd56d25a739829a
Felipe <felipetiburcioviana@gmail.com>
2026-08-20 18:05:30 -0400

    added implementation for HTTP GET AND GET by ID routes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,14 +12,18 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(JSON.stringify(books, NULL, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    
+    const isbn = req.params.isbn;
+
+    let filtered_book = books[isbn]
+
+
+  return res.status(300).json({ book: filtered_book });
  });
   
 // Get book details based on author
