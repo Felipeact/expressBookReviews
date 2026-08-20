3b2ab4a710a7c045de13d1af23b8e4e65e8227e2
Felipe <felipetiburcioviana@gmail.com>
2026-08-20 18:14:27 -0400

    getting the book details based on the author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,7 +12,7 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  return res.status(JSON.stringify(books, NULL, 4));
+  return res.status(300).json({ books });
 });
 
 // Get book details based on ISBN
@@ -28,8 +28,18 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    
+    const author = req.params.author;
+    const results = [];
+    
+    for (const id in books) {
+        if (books[id].author == author )
+        {
+            results.push(books[id]);
+        }
+    }
+
+  return res.status(300).json(results);
 });
 
 // Get all books based on title
