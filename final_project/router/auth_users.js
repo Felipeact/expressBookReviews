e6d4523c401043935b9628f9843055be6abdc673
Felipe Viana <felipetiburcioviana@gmail.com>
2026-08-21 13:44:16 -0400

    Complete the code for logging in as a registered user

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,12 +6,29 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username) => { //returns boolean
-    //write code to check is the username is valid
+    let userswithsamename = users.filter((user) => {
+        return user.username === username;
+    });
+    // Return true if any user with the same username is found, otherwise false
+    if (userswithsamename.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-const authenticatedUser = (username, password) => { //returns boolean
-    //write code to check if username and password match the one we have in records.
+const authenticatedUser = (username, password) => { 
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+    // Return true if any valid user is found, otherwise false
+    if (validusers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
+    
 
 //only registered users can login
 regd_users.post("/login", (req, res) => {
@@ -41,7 +58,9 @@ regd_users.post("/login", (req, res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-    //Write your code here
+
+    
+
     return res.status(300).json({ message: "Yet to be implemented" });
 });
 
